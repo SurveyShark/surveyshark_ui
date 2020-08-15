7d5b6b15a0edb1b033c422e091136c8e2fad6514
RadeshShiwlochan <radesh0430@gmail.com>
2020-08-15 18:17:01 -0400

    add Next and Previous Button components

--- src/client/components/QuestionPage.js
+++ src/client/components/QuestionPage.js
@@ -1,19 +1,21 @@
 import React from 'react'
+import NextButton from './NextButton'
+import PreviousButton from './PreviousButton'
 
-const QuestionPage = () => {
-  return (
-    <div>
-      <h1>Question 1</h1>
-      <form>
-        <input type="radio" id="A" />
-        <label htmlFor="A">Choice A</label>
-        <input type="radio" id="B" />
-        <label htmlFor="B">Choice B</label>
-        <input type="radio" id="C" />
-        <label htmlFor="C">Choice C</label>
-      </form>
-    </div>
-  )
-}
+const Question = () => (
+  <div>
+    <h1>Question 1</h1>
+    <form>
+      <input type="radio" id="A" />
+      <label htmlFor="A">Choice A</label>
+      <input type="radio" id="B" />
+      <label htmlFor="B">Choice B</label>
+      <input type="radio" id="C" />
+      <label htmlFor="C">Choice C</label>
+    </form>
+    <PreviousButton previousLinkUrl="./" />
+    <NextButton nextLinkUrl="./question-two" />
+  </div>
+)
 
-export default QuestionPage
+export default Question
