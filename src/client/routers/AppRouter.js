8723a852c0a31ef711ee2e2eee3c5c199fc6891d
RadeshShiwlochan <radesh0430@gmail.com>
2020-08-23 19:36:42 -0400

    add other question components

--- src/client/routers/AppRouter.js
+++ src/client/routers/AppRouter.js
@@ -3,6 +3,16 @@ import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom'
 import Headers from '../components/Header'
 import MainContent from '../components/MainContent'
 import QuestionOne from '../views/QuestionOne'
+import QuestionTwo from '../views/QuestionTwo'
+import QuestionThree from '../views/QuestionThree'
+import QuestionFour from '../views/QuestionFour'
+import QuestionFive from '../views/QuestionFive'
+import QuestionSix from '../views/QuestionSix'
+import QuestionSeven from '../views/QuestionSeven'
+import QuestionEight from '../views/QuestionEight'
+import QuestionNine from '../views/QuestionNine'
+import QuestionTen from '../views/QuestionTen'
+import Submission from '../views/Submission'
 
 const AppRouter = () => (
   <BrowserRouter>
@@ -11,6 +21,16 @@ const AppRouter = () => (
       <Switch>
         <Route path="/" component={MainContent} exact={true} />
         <Route path="/question-one" component={QuestionOne} />
+        <Route path="/question-two" component={QuestionTwo} />
+        <Route path="/question-three" component={QuestionThree} />
+        <Route path="/question-four" component={QuestionFour} />
+        <Route path="/question-five" component={QuestionFive} />
+        <Route path="/question-six" component={QuestionSix} />
+        <Route path="/question-seven" component={QuestionSeven} />
+        <Route path="/question-eight" component={QuestionEight} />
+        <Route path="/question-nine" component={QuestionNine} />
+        <Route path="/question-ten" component={QuestionTen} />
+        <Route path="/submission" component={Submission} />
       </Switch>
     </div>
   </BrowserRouter>
---
+++ src/client/views/Submission.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Submission = () => (
+  <div>
+    <h1>Submission</h1>
+    <h3>You are about to submit your answers</h3>
+    <button>Submit</button>
+  </div>
+)
+
+export default Submission
